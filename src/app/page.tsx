95d3e9c1356d55d92c39880efeae9d55ef4af297
igor <prekop.igor@gmail.com>
2023-12-12 08:02:36 +0100

    12 beginning class

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { Products, StaticMessages } from '@/components/fetch-data'
-import { DataList } from '@/components/data-list'
+import { createDB } from '@/lib/db'
 import Link from 'next/link'
 import React from 'react'
 import { redirect } from 'next/navigation'
@@ -7,13 +7,29 @@ import { redirect } from 'next/navigation'
 type Props = {
   searchParams: { page: string }
 }
-export default function Home({ searchParams }: Props) {
+
+async function maxItems() {
+  const db = createDB()
+  const products = await db
+    .selectFrom('product')
+    .select((eb) => eb.fn.countAll<number>().as('count'))
+    .execute()
+  return Math.ceil(products[0].count / 9)
+}
+
+export default async function Home({ searchParams }: Props) {
+  const maxPage = await maxItems()
   const page = parseInt(searchParams.page)
+
   if (isNaN(page)) {
     redirect('/?page=1')
   }
   if (page <= 0) {
     redirect('/?page=1')
+  }
+
+  if (page > maxPage) {
+    redirect(`/?page=${maxPage}`)
   }
   return (
     <>
@@ -23,6 +39,7 @@ export default function Home({ searchParams }: Props) {
       >
         Previous Page
       </Link>
+
       <Link
         className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         href={`?page=${page + 1}`}
